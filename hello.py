91466e70a131691f10a436085bbd32b31c1fea29
nakatuba <nakatuba@gmail.com>
2020-12-21 22:38:44 +0900

    Modify request method

--- hello.py
+++ hello.py
@@ -15,78 +15,78 @@ def hello_world():
     return redirect(url_for('login'))
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
-
-
-@app.route('/auth', methods=['POST'])
-def auth():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-
-    c = conn.cursor()
-
-    username = request.form.get('username')
-    password = request.form.get('password')
-
-    c.execute("select * from users where username=? and password=?",
-              (username, hash(password)))
-    user = c.fetchone()
-
-    conn.close()
-
-    if user is None:
-        return render_template('login.html', error="ユーザーネームまたはパスワードが違います")
+    if request.method == 'GET':
+        return render_template('login.html')
     else:
-        return redirect(url_for('chat', sender=user['username']))
+        conn = sqlite3.connect('database.db')
+        conn.row_factory = sqlite3.Row
+
+        c = conn.cursor()
+
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        c.execute("select * from users where username=? and password=?",
+                  (username, hash(password)))
+        user = c.fetchone()
+
+        conn.close()
+
+        if user is None:
+            return render_template('login.html',
+                                   error="ユーザーネームまたはパスワードが違います")
+        else:
+            return redirect(url_for('chat', sender=user['username']), code=307)
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        conn = sqlite3.connect('database.db')
+        conn.row_factory = sqlite3.Row
+
+        c = conn.cursor()
+
+        icon = request.files.get('icon')
+        icon.save("./static/" + icon.filename)
+
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        try:
+            c.execute("insert into users values (?, ?, ?)",
+                      (icon.filename, username, hash(password)))
+        except sqlite3.IntegrityError:
+            return render_template('register.html',
+                                   error="同じユーザーネームがすでに登録済みです")
+
+        conn.commit()
+
+        conn.close()
+
+        return redirect(url_for('chat', sender=username), code=307)
 
 
-@app.route('/create', methods=['POST'])
-def create():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-
-    c = conn.cursor()
-
-    icon = request.files.get('icon')
-    icon.save("./static/" + icon.filename)
-
-    username = request.form.get('username')
-    password = request.form.get('password')
-
-    try:
-        c.execute("insert into users values (?, ?, ?)",
-                  (icon.filename, username, hash(password)))
-    except sqlite3.IntegrityError:
-        return render_template('register.html', error="同じユーザーネームがすでに登録済みです")
-
-    conn.commit()
-
-    conn.close()
-
-    return redirect(url_for('chat', sender=username))
-
-
-@app.route('/chat/<sender>', methods=['GET', 'POST'])
+@app.route('/chat/<sender>', methods=['POST'])
 def chat(sender):
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
 
     c = conn.cursor()
 
-    receiver = request.form.get('receiver')
     text = request.form.get('text')
+
     if text and not text.isspace():
+        receiver = request.form.get('receiver')
         date = datetime.datetime.now().strftime('%Y年%m月%d日')
         time = datetime.datetime.now().strftime('%H:%M')
+
         c.execute("insert into messages values (?, ?, ?, ?, ?)",
-                  (sender, receiver, text, date, time))
+                  (sender, text, receiver, date, time))
 
         conn.commit()
 
